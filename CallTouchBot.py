d1a015b0bcaf9a2f1f3ca9d55d7717490bd203be
Alexandr Agafonov <agafonovfm@gmail.com>
2022-02-18 11:43:59 +0300

    asynd version

--- CallTouchBot.py
+++ CallTouchBot.py
@@ -8,26 +8,32 @@ from aiogram.dispatcher import FSMContext
 from tg_keyboard import main_keyboard, calltouch_keyboard
 
 # импортируем внутренни функции
-from calltouch_functions import validate_creds, get_today_data
-from db_functions import create_new_ct, list_of_accounts, remove_account, validate_cabinet_id
+from calltouch_functions_async import CallTouchFunctions
+from db_functions_async import DbFunctions
 
 # импорт других библиотек
 from datetime import datetime
 import itertools
+import asyncio
+import nest_asyncio
+
 
 token = '1974255060:AAFosMbvKycoKRydyPdi4yjflv8031SW4mE' # боевой
 #token = '1938283222:AAEe7C80RbtpAjW7BVBzt6qISW8VnzIpg0A'  # тестовый
 bot = Bot(token=token)
 dp = Dispatcher(bot, storage=MemoryStorage())
+nest_asyncio.apply()  # для добавления задач в ассинхрон
 
 # глобальные переменные
 ct_creds = dict()  # сюда кладем крденшалы для сохранения колтача
+
 
 # Приветственный блок
 @dp.message_handler(commands=['start'], state='*')  # приветствуем и показываем клавиатуру
 async def start_message(message: types.Message, state: FSMContext):
     await state.reset_state()
-    message_text = 'Привет! Я вышлю Вам статистику из CallTouch в любое время!\n'\
+    message_text = 'Привет!\n' \
+                   'Я вышлю Вам статистику из CallTouch в любое время!\n'\
                    'Выберете нужный пункт меню'
     await bot.send_message(message.from_user.id, message_text, reply_markup=main_keyboard)
 
@@ -35,13 +41,13 @@ async def start_message(message: types.Message, state: FSMContext):
 # блок создания CT
 @dp.callback_query_handler(lambda c: c.data == 'create_creds')
 async def create_creds_get_id(callback_query: types.CallbackQuery):
-    message_text = 'Что бы завести кабинет необходимо ввести:\n'\
+    message_text = 'Все данные для подключения, можно найти в личном кабинете Calltouch:\n'\
+                   'Интеграция => API и Webhooks\n' \
+                   'Что бы завести кабинет, вам понадобится:\n'\
                    '1. ID своего кабинета Calltouch,\n'\
                    '2. Ссылку на сервер,\n'\
-                   '3. Токен доступа\n'\
-                   'Все это можно найти в личном кабинете Calltouch\n'\
-                   'Интеграция => API и Webhooks\n' \
-                   'Введите /start что бы вернуться назад'
+                   '3. Токен доступа\n' \
+                   'Или /start что бы вернуться назад\n'
     await bot.answer_callback_query(callback_query.id)
     await bot.send_message(callback_query.from_user.id, message_text)
     await bot.send_message(callback_query.from_user.id, 'Введите ID кабинета CallTouch')
@@ -50,56 +56,91 @@ async def create_creds_get_id(callback_query: types.CallbackQuery):
 
 # получение ID кабинета calltouch
 @dp.message_handler(state=Actions.create_ct_get_id)
-async def get_ct_id(message: types.Message):
-    ct_creds['cabinet_id'] = message.text
+async def get_ct_id(message: types.Message, state: FSMContext):
+    # сохраняем ID введенного кабинета
+    async with state.proxy() as data:
+        data['cabinet_id'] = message.text
+
     await Actions.create_ct_get_server.set()
     await bot.send_message(message.from_user.id, 'Введите ссылку на сервер')
 
 
 # получение ноды calltouch
 @dp.message_handler(state=Actions.create_ct_get_server)
-async def get_ct_server(message: types.Message):
-    ct_creds['server'] = message.text
+async def get_ct_server(message: types.Message, state: FSMContext):
+    # сохраняем ноду введенного кабинета
+    async with state.proxy() as data:
+        data['server'] = message.text
     await Actions.create_ct_get_token.set()
     await bot.send_message(message.from_user.id, 'Введите токен')
 
 
-# получение ноды calltouch, валидация и запись данных в БД
+# получение токена calltouch, валидация и запись данных в БД
 @dp.message_handler(state=Actions.create_ct_get_token)
 async def get_ct_token(message: types.Message, state: FSMContext):
-    ct_creds['token'] = message.text
-    ct_creds['create_date'] = datetime.now().strftime('%Y-%m-%d %H:%M')
-    ct_creds['user_id'] = message.from_user.id
+
+    async with state.proxy() as data:
+        data['token'] = message.text
+        data['create_date'] = datetime.now().strftime('%Y-%m-%d %H:%M')
+        data['user_id'] = message.from_user.id
 
     try:
-        ct_creds['user_name'] = message.from_user.first_name
+        async with state.proxy() as data:
+            data['user_name'] = message.from_user.first_name
     except ValueError:
-        ct_creds['user_name'] = 'unknown'
+        async with state.proxy() as data:
+            data['user_name'] = 'unknown'
 
     try:
-        ct_creds['user_surname'] = message.from_user.last_name
+        async with state.proxy() as data:
+            data['user_surname'] = message.from_user.last_name
     except ValueError:
-        ct_creds['user_surname'] = 'unknown'
+        async with state.proxy() as data:
+            data['user_surname'] = 'unknown'
 
-    if validate_creds(ct_creds['cabinet_id'], ct_creds['server'], ct_creds['token']) != 'Данные проверены!':
-        ct_creds.clear()
+    # добавляем задачу ассинхронно
+    loop = asyncio.get_running_loop()
+    run_func = loop.create_task(CallTouchFunctions.validate_creds(data['cabinet_id'], data['server'], data['token']))
+    loop.run_until_complete(run_func)
+    result = run_func.result()
+
+    if result != 'ok':
+        # очищаем креды колтача в случае ошибки
+        async with state.proxy() as data:
+            data.clear()
         message_text = 'Введенные данные некорректны.\n'\
                        'Попробуйте еще раз'
         await bot.send_message(message.from_user.id, message_text, reply_markup=main_keyboard)
         await state.reset_state()
     else:
-        create_new_ct(ct_creds)
-        message_text = 'Данные успешно сохранены!\n'\
-                       'Теперь вы можете получать статистику'
-        await bot.send_message(message.from_user.id, message_text, reply_markup=main_keyboard)
-        await state.finish()
+        # добавляем задачу ассинхронно
+        loop = asyncio.get_running_loop()
+        run_func = loop.create_task(DbFunctions.create_new_ct(data))
+        loop.run_until_complete(run_func)
+        result = run_func.result()
+
+        if result == 'already exists':
+            message_text = 'Данный кабинет уже подключен'
+            await bot.send_message(message.from_user.id, message_text, reply_markup=main_keyboard)
+            await state.finish()
+
+        else:
+            message_text = 'Данные успешно сохранены!\n'\
+                           'Теперь вы можете получать статистику'
+            await bot.send_message(message.from_user.id, message_text, reply_markup=main_keyboard)
+            await state.finish()
 
 
 # запрашиваем список аккаунтов CT для удаления
 @dp.callback_query_handler(lambda c: c.data == 'remove_creds')
 async def show_creds_to_remove(callback_query: types.CallbackQuery, state: FSMContext):
     await bot.answer_callback_query(callback_query.id)
-    calltouch_accounts = list_of_accounts(callback_query.from_user.id)
+
+    # добавляем задачу ассинхронно
+    loop = asyncio.get_running_loop()
+    run_func = loop.create_task(DbFunctions.list_of_accounts(callback_query.from_user.id))
+    loop.run_until_complete(run_func)
+    calltouch_accounts = run_func.result()
 
     if len(calltouch_accounts) == 0:
         message_text = 'У вас нет аккаунтов CallTouch\n' \
@@ -121,15 +162,23 @@ async def show_creds_to_remove(callback_query: types.CallbackQuery, state: FSMCo
 # удаляем список
 @dp.message_handler(state=Actions.remove_ct_creds)
 async def remove_ct_creds(message: types.Message, state: FSMContext):
-    # делаем плоский список из того что возращает функция list_of_account
-    calltouch_accounts = list(itertools.chain.from_iterable(list_of_accounts(message.from_user.id)))
+
+    # добавляем задачу ассинхронно
+    loop = asyncio.get_running_loop()
+    run_func = loop.create_task(DbFunctions.list_of_accounts(message.from_user.id))
+    loop.run_until_complete(run_func)
+    calltouch_accounts = list(itertools.chain.from_iterable(run_func.result()))  # делаем плоский список
 
     if message.text not in calltouch_accounts:
         message_text = 'Такого аккаунта нет в списке.\n'\
                        'Введите аккаунт из списка выше или нажмите /start что бы вернуться назад'
         await bot.send_message(message.from_user.id, message_text)
     else:
-        remove_account(message.from_user.id, message.text)
+        # добавляем задачу ассинхронно
+        loop = asyncio.get_running_loop()
+        run_func = loop.create_task(DbFunctions.remove_account(message.from_user.id, message.text))
+        loop.run_until_complete(run_func)
+
         message_text = f'Аккаунт {message.text} удален!\n' \
                        f'Нажмите /start что бы вернуться в меню'
         await bot.send_message(message.from_user.id, message_text)
@@ -140,7 +189,12 @@ async def remove_ct_creds(message: types.Message, state: FSMContext):
 @dp.callback_query_handler(lambda c: c.data == 'show_creds')
 async def show_my_creds(callback_query: types.CallbackQuery):
     await bot.answer_callback_query(callback_query.id)
-    calltouch_accounts = list_of_accounts(callback_query.from_user.id)
+
+    # добавляем задачу ассинхронно
+    loop = asyncio.get_running_loop()
+    run_func = loop.create_task(DbFunctions.list_of_accounts(callback_query.from_user.id))
+    loop.run_until_complete(run_func)
+    calltouch_accounts = list(itertools.chain.from_iterable(run_func.result()))
 
     if len(calltouch_accounts) == 0:
         message_text = 'У вас нет аккаунтов CallTouch\n' \
@@ -150,7 +204,7 @@ async def show_my_creds(callback_query: types.CallbackQuery):
         message_text = 'ID ваших кабинетов:\n'
 
         for account in calltouch_accounts:
-            message_text += account[0] + '\n'
+            message_text += account + '\n'
 
         message_text += 'Нажмите /start что бы вернуться назад'
         await bot.send_message(callback_query.from_user.id, message_text)
@@ -158,9 +212,14 @@ async def show_my_creds(callback_query: types.CallbackQuery):
 
 # выводим список кабинетов при запросе статистики
 @dp.callback_query_handler(lambda c: c.data == 'get_call_stat')
-async def show_my_creds(callback_query: types.CallbackQuery):
+async def show_my_creds(callback_query: types.CallbackQuery, state: FSMContext):
     await bot.answer_callback_query(callback_query.id)
-    calltouch_accounts = list_of_accounts(callback_query.from_user.id)
+
+    # добавляем задачу ассинхронно
+    loop = asyncio.get_running_loop()
+    run_func = loop.create_task(DbFunctions.list_of_accounts(callback_query.from_user.id))
+    loop.run_until_complete(run_func)
+    calltouch_accounts = list(itertools.chain.from_iterable(run_func.result()))
 
     if len(calltouch_accounts) == 0:
         message_text = 'У вас нет аккаунтов CallTouch\n' \
@@ -168,17 +227,16 @@ async def show_my_creds(callback_query: types.CallbackQuery):
         await bot.send_message(callback_query.from_user.id, message_text)
 
     elif len(calltouch_accounts) == 1:
-        global global_cabinet_id
-        global_cabinet_id = list(itertools.chain.from_iterable(calltouch_accounts))[0]
+        async with state.proxy() as data:
+            data['cabinet_id'] = calltouch_accounts[0]
         message_text = 'Выберете пункт меню:'
         await bot.send_message(callback_query.from_user.id, message_text, reply_markup=calltouch_keyboard)
         await Actions.get_statistic.set()
-
     else:
         message_text = 'Введите ID кабинета по которому хотели бы получать статистику:\n'
 
         for account in calltouch_accounts:
-            message_text += account[0] + '\n'
+            message_text += account + '\n'
 
         message_text += 'Или нажмите /start что бы вернуться назад'
         await bot.send_message(callback_query.from_user.id, message_text)
@@ -187,16 +245,23 @@ async def show_my_creds(callback_query: types.CallbackQuery):
 
 # выбор кабинета если их больше одного
 @dp.message_handler(state=Actions.choose_cabinet_id)
-async def choose_cabinet_id(message: types.Message):
+async def choose_cabinet_id(message: types.Message, state: FSMContext):
     cabinet_id = message.text
-    account_list = list(itertools.chain.from_iterable(validate_cabinet_id(message.from_user.id, cabinet_id)))
+
+    # добавляем задачу ассинхронно
+    loop = asyncio.get_running_loop()
+    run_func = loop.create_task(DbFunctions.validate_cabinet_id(message.from_user.id, cabinet_id))
+    loop.run_until_complete(run_func)
+    account_list = list(itertools.chain.from_iterable(run_func.result()))
+
     if cabinet_id not in account_list:
         message_text = 'Аккаунтов CallTouch с таким ID не найдено.\n' \
                        'Нажмите /start что бы вернуться назад'
         await bot.send_message(message.from_user.id, message_text)
     else:
-        global global_cabinet_id
-        global_cabinet_id = cabinet_id
+        async with state.proxy() as data:
+            data['cabinet_id'] = cabinet_id
+
         message_text = 'Выберете пункт меню:'
         await bot.send_message(message.from_user.id, message_text, reply_markup=calltouch_keyboard)
         await Actions.get_statistic.set()
@@ -204,34 +269,58 @@ async def choose_cabinet_id(message: types.Message):
 
 # Статистика за сегодня по выбранному ЛК
 @dp.callback_query_handler(lambda c: c.data == 'get_today_stat', state=Actions.get_statistic)
-async def get_today_calls(callback_query: types.CallbackQuery):
+async def get_today_calls(callback_query: types.CallbackQuery, state: FSMContext):
     await bot.answer_callback_query(callback_query.id)
 
-    result = get_today_data(callback_query.from_user.id, global_cabinet_id, 0)
+    async with state.proxy() as data:
+        data['days_ago'] = 0
 
-    message_text = f'Статистика за {result["date"]}:\n' \
-                   f'Всего звонков: {result["total_calls"]}\n' \
-                   f'Уникальных звонков: {result["unique_calls"]}\n' \
-                   f'Целевых звонков: {result["target_calls"]}\n' \
-                   f'Уникально-целевых звонков: {result["unique_target_calls"]}'
+    # добавляем задачу ассинхронно
+    loop = asyncio.get_running_loop()
+    run_func = loop.create_task(
+        CallTouchFunctions.get_n_days_ago_data(callback_query.from_user.id, data['cabinet_id'], data['days_ago']))
+    loop.run_until_complete(run_func)  # ошибка но почему - хер знает
+    result = run_func.result()
 
-    await bot.send_message(callback_query.from_user.id, message_text, reply_markup=calltouch_keyboard)
+    if result == 'error':
+        message_text = f'Возникла ошибка.\nПопробуйте удалить кабинет {data["cabinet_id"]} и попробуйте снова'
+        await bot.send_message(callback_query.from_user.id, message_text, reply_markup=calltouch_keyboard)
+    else:
+        message_text = f'Статистика за {result["date"]}:\n' \
+                       f'Всего звонков: {result["total_calls"]}\n' \
+                       f'Уникальных звонков: {result["unique_calls"]}\n' \
+                       f'Целевых звонков: {result["target_calls"]}\n' \
+                       f'Уникально-целевых звонков: {result["unique_target_calls"]}'
+
+        await bot.send_message(callback_query.from_user.id, message_text, reply_markup=calltouch_keyboard)
 
 
 # Статистика за вчера по выбранному ЛК
 @dp.callback_query_handler(lambda c: c.data == 'get_yesterday_stat', state=Actions.get_statistic)
-async def get_today_calls(callback_query: types.CallbackQuery):
+async def get_today_calls(callback_query: types.CallbackQuery, state: FSMContext):
     await bot.answer_callback_query(callback_query.id)
 
-    result = get_today_data(callback_query.from_user.id, global_cabinet_id, 1)
+    async with state.proxy() as data:
+        data['days_ago'] = 1
 
-    message_text = f'Статистика за {result["date"]}:\n' \
-                   f'Всего звонков: {result["total_calls"]}\n' \
-                   f'Уникальных звонков: {result["unique_calls"]}\n' \
-                   f'Целевых звонков: {result["target_calls"]}\n' \
-                   f'Уникально-целевых звонков: {result["unique_target_calls"]}'
+    # добавляем задачу ассинхронно
+    loop = asyncio.get_running_loop()
+    run_func = loop.create_task(
+        CallTouchFunctions.get_n_days_ago_data(callback_query.from_user.id, data['cabinet_id'], data['days_ago']))
+    loop.run_until_complete(run_func)  # ошибка но почему - хер знает
+    result = run_func.result()
 
-    await bot.send_message(callback_query.from_user.id, message_text, reply_markup=calltouch_keyboard)
+    if result == 'error':
+        message_text = f'Возникла ошибка.\nПопробуйте удалить кабинет {data["cabinet_id"]} и попробуйте снова'
+        await bot.send_message(callback_query.from_user.id, message_text, reply_markup=calltouch_keyboard)
+    else:
+        message_text = f'Статистика за {result["date"]}:\n' \
+                       f'Всего звонков: {result["total_calls"]}\n' \
+                       f'Уникальных звонков: {result["unique_calls"]}\n' \
+                       f'Целевых звонков: {result["target_calls"]}\n' \
+                       f'Уникально-целевых звонков: {result["unique_target_calls"]}'
+
+        await bot.send_message(callback_query.from_user.id, message_text, reply_markup=calltouch_keyboard)
 
 
 # функция возврата в главное меню
@@ -243,10 +332,25 @@ async def get_back_to_upper_menu(callback_query: types.CallbackQuery, state: FSM
     await state.reset_state()
 
 
+# выгрузка БД для "своих"
+@dp.message_handler(commands=['list'], state='*')
+async def start_message(message: types.Message, state: FSMContext):
+    message_text = 'Формирую CSV-файл'
+    await bot.send_message(message.from_user.id, message_text)
+
+    # добавляем задачу ассинхронно
+    loop = asyncio.get_running_loop()
+    run_func = loop.create_task(DbFunctions.download_db(message.from_user.id))
+    loop.run_until_complete(run_func)
+    result = run_func.result()
+
+    if result != 'ok':
+        pass
+    else:
+        file_name = open('db_file.csv', 'rb')
+        await bot.send_document(message.from_user.id, file_name)
+
+
 executor.start_polling(dp, skip_updates=True)
-# ветка с заведеним колтача полностью готова =)
-# ветка удаления аккаунта готова
-# ветка вывода списка аккаунтон готова. по идее можно делать сбор статы)
-# видимо нужно вынести id кабинета в какую то глобальную переменную
-# кажись эта балалайка работает =)
-# осталось выложить)
+# збс, все четенько переведено в ассинхрон
+# при добавлении ЛК надо првоерять нет ли такого уже в БД
